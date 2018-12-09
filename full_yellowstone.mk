a9bc968f1e6c1f9a421d98f46aad3be383a40f07
Nolen Johnson <johnsonnolen@gmail.com>
2018-12-09 04:19:21 -0500

    yellowstone: Add full_yellowstone build target
    * Inherit full_base as opposed to aosp_base, as this device was never
      officially supported by AOSP.
    * Inherit proprietary vendor blobs.
    
    Change-Id: Idc37a1433f015b66ab187b6af09d3c0db98b2f95

--- full_yellowstone.mk
+++ full_yellowstone.mk
@@ -1,4 +1,5 @@
-# Copyright (C) 2014 The Android Open Source Project
+#
+# Copyright (C) 2014 The Android Open-Source Project
 # Copyright (C) 2018 Tegra Development
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,7 +12,16 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License
+# limitations under the License.
+#
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_yellowstone.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, device/google/yellowstone/device.mk)
+
+PRODUCT_NAME := full_yellowstone
+PRODUCT_DEVICE := yellowstone
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Project Tango Tablet Development Kit
+PRODUCT_MANUFACTURER := Google
+
+$(call inherit-product, vendor/google/yellowstone/yellowstone-vendor.mk)
