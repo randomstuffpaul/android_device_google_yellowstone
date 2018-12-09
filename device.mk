0aa5027133afe0d9cb731ff83846d47b5f24b2b9
Nolen Johnson <johnsonnolen@gmail.com>
2018-12-09 11:47:12 +0000

    yellowstone: Add media codecs/profiles
    * Pulled from stock.
    * Opted to include Google media codecs/profiles as well, as this is technically a
      Google-made device.

--- device.mk
+++ device.mk
@@ -24,6 +24,14 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
     libbt-vendor
 
+# Codecs
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+    $(LOCAL_PATH)/media/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/media/media_profiles.xml:system/etc/media_profiles.xml
+
 # HIDL
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/manifest.xml:system/manifest.xml
